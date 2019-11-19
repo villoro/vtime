72e1886d78b70078868927085c129350f2bf39a7
Arnau Villoro <arnau@villoro.com>
2019-11-19 14:57:17 +0100

    [NEW] v_time functions

---
+++ v_time/v_time.py
@@ -0,0 +1,95 @@
+"""
+    Timeit decorator.
+    This decorator will run n_times a function and output the execution times.
+
+    More info about decorators at: https://develop.villoro.com/post/decorators
+
+    It uses 'perf_counter' instead of 'time' since it has more precission
+"""
+
+import functools
+from time import perf_counter
+
+import yaml
+
+
+def timeit(func):
+    """
+        Decorator that prints the execution time of a function. Example:
+
+        from time import sleep
+
+        @timeit
+        def sleep_one_sec():
+            sleep(1)
+    """
+
+    @functools.wraps(func)
+    def timed(*args, **kwa):
+        """ Prints the execution time of the decorated function """
+        t0 = perf_counter()
+
+        result = func(*args, **kwa)
+
+        total_time = round((perf_counter() - t0) / 60, 2)
+        print("timeit: '{}' in {:.2f} min".format(func.__name__, total_time))
+
+        return result
+
+    return timed
+
+
+def stimeit(output_func=print):
+    """
+        Decorator that displays the execution time of a function.
+        Allows to use custom output functions (like print, log.info...)
+
+        Example:
+
+        from time import sleep
+
+        @stimeit(log.info)
+        def sleep_one_sec():
+            sleep(1)
+    """
+
+    def timeit_decorator(func):
+        """ Decorator to time the execution of a function """
+
+        @functools.wraps(func)
+        def timed_execution(*args, **kwa):
+            """ Prints the execution time of the decorated function """
+            t0 = time()
+
+            result = func(*args, **kwa)
+
+            total_time = time() - t0
+
+            output_func("stimeit: '{}' in {:.2f}".format(func.__name__, round(total_time / 60, 2)))
+
+            return result
+
+        return timed_execution
+
+    return timeit_decorator
+
+
+def mesure(func, n_iterations=10, *args, **kwargs):
+    """ Allows to time a function n times """
+
+    out = []
+    for _ in range(n_iterations):
+        t0 = perf_counter()
+        result = func(*args, **kwargs)
+        out.append(perf_counter() - t0)
+
+    return out
+
+
+def store_results(data, test_name):
+    """ Store results as a yaml """
+
+    with open(f"results/{test_name}.yaml", "w") as outfile:
+        yaml.dump(data, outfile, default_flow_style=False)
+
+    print(f"\nAll tests done for {test_name}")
